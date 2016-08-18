2606b559e3e7a56de009e90724a7a93917e63b55
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2016-08-18 17:39:58 +0000

    More updates to submission form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :content, presence: true
+    validate do
+        valid_location
+    end
+    
+    
+    
+    private
+        def valid_location
+           self.errors.add(:base, "test")
+        end
 end
